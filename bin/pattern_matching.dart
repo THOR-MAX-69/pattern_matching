f6a08bac6e3d97a8a46378af0ec15391e64a1b78
Ahmed Khan <khanbahadur221100@gmail.com>
2025-06-26 23:29:12 +0500

    Second Commit

--- bin/pattern_matching.dart
+++ bin/pattern_matching.dart
@@ -1,31 +1,31 @@
 import 'pattern_match.dart';
 void main(List<String> arguments) {
-  // var list = [1,2,3];
-  // var person = ("Ahmed",20);
-  // print(describePerson(person));
-  // print(listLengthCategory(list));
+  var list = [1,2,3];
+  var person = ("Ahmed",20);
+  print(describePerson(person));
+  print(listLengthCategory(list));
 
-//   List<Map<String, Object>> students = [
-//   {"name": "Alice", "score": 95},
-//   {"name": "Bob", "score": 88},
-//   {"name": "Charlie", "score": 91},
-//   {"name": "Diana", "score": 72},
-//   {"name": "Ethan", "score": 99},
-//   {"name": "Fiona", "score": 85},
-// ];
+  List<Map<String, Object>> students = [
+  {"name": "Alice", "score": 95},
+  {"name": "Bob", "score": 88},
+  {"name": "Charlie", "score": 91},
+  {"name": "Diana", "score": 72},
+  {"name": "Ethan", "score": 99},
+  {"name": "Fiona", "score": 85},
+];
 
-// print(topScorers(students));
+print(topScorers(students));
 
-// List<(String, int)> people = [
-//   ("Alice", 22),
-//   ("Bob", 17),
-//   ("Charlie", 19),
-//   ("Diana", 16),
-//   ("Ethan", 25),
-//   ("Fiona", 18),
-// ];
+List<(String, int)> people = [
+  ("Alice", 22),
+  ("Bob", 17),
+  ("Charlie", 19),
+  ("Diana", 16),
+  ("Ethan", 25),
+  ("Fiona", 18),
+];
 
-// print(adults(people));
+print(adults(people));
 
 List<((String, int), String)> peopleList = [
   (("Alice", 22), "Lahore"),
